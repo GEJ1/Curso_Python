b7d901db579330eb9c61be50fd19b3f822924090
GEJ1 <gustavojuantorena@hotmail.com>
2017-03-31 18:18:31 -0300

    "Materias" casi terminado

--- 2.0_tipos_de_datos.py
+++ 2.0_tipos_de_datos.py
@@ -7,13 +7,6 @@
 
 # Se requiere ingresar por pantalla notas de alumnos y sus respectivos nombres. Los nombres deberán contener minimamente 5 caracteres y su nota será entre 0 y 10.
 Es necesario calcular el promedio de los alumnos.
-'''
-
-'''
-boletin[nombre] = nota
-nota = int(nota)
-nota = boletin.values()
-'''
 
 def promedio(nota):
 
@@ -36,6 +29,7 @@ def entrada_boletin(otro_dato):
 
     elif otro_dato == 'no':
         print (promedio(nota))
+#main
 
 # Creo un diccionario vacio
 boletin = {}
@@ -49,11 +43,56 @@ if otro_dato == 'si':
         print(promedio(nota))
 elif otro_dato == 'no':
     print(promedio(nota))
+    
+#################################################################################################################################################
 
 
 
-''''
-- Se requiere ingresar por pantalla materias de la carrera. La materia se compone con: número de 5 cifras de identificación única, un nombre y un listado de alumnos (con sus notas).
+- Se requiere ingresar por pantalla materias de la carrera. La materia se compone con: número de 5 cifras de identificación 
+única, un nombre y un listado de alumnos (con sus notas).
+'''
+
+def entrada_boletin(otro_dato):
+    nombre = input('ingrese el nombre del alumno. Minimo 5 caracteres\n')
+    if len(nombre) > 4:
+        nota = input('ingrese la nota del alumno con un valor entre 0 y 10\n')
+        nota = int(nota)
+        if -1 < nota < 11:
+            nota = int(nota)
+            boletin[nombre] = nota
+
+
+
+            materias.append(nota)
+            otro_dato = input('Desea agregar otro alumno? Escriba si o no y presione enter.\n')
+
+    if otro_dato == 'si':
+        return entrada_boletin(otro_dato)
+
+    elif otro_dato == 'no':
+        print (materias)
+
+materias = []
+boletin = {}
+otro_dato = 'asd'
+
+numero = input('ingrese el numero de materia. El mismo debera tener 5 digitos.\n')
+materias.append(numero)
+nombre = input('ingrese el nombre de la materia. El mismo debera tener 5 digitos.\n')
+materias.append(nombre)
+entrada_boletin(otro_dato)
+if otro_dato == 'si':
+    entrada_boletin(otro_dato)
+    if otro_dato == 'no':
+        print (materias)
+elif otro_dato == 'no':
+    print (materias)
+
+
+
+
+
+'''
 
 - Dada una cadena de texto de 10 a 20 caracteres ingresada por el usuario quedarse con los primeros 3 y los ultimos 5.
 
@@ -61,4 +100,6 @@ elif otro_dato == 'no':
     - El programa mostrara las opciones> agregar, editar, borrar, mostrar y salir
     agregar, agenda un contacto (email, telefono, nombre, domicilio, edad y dni)
     editar, permite modificar cualquiera de los contactos seleccionando su email.
-    borrar, elimina un contacto. '''
+    borrar, elimina un contacto.
+
+'''
